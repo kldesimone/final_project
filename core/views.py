d7d791ce259d1ef4522830c25cd968ad200b6a60
Kristi <kldesimone@quinnipiac.edu>
2015-11-10 19:46:19 +0000

    Add user profile links

--- core/views.py
+++ core/views.py
@@ -122,3 +122,12 @@ class UserDetailView(DetailView):
   slug_field = 'username'
   template_name = 'user/user_detail.html'
   context_object_name = 'user_in_view'
+  
+  def get_context_data(self, **kwargs):
+      context = super(UserDetailView, self).get_context_data(**kwargs)
+      user_in_view = User.objects.get(username=self.kwargs['slug'])
+      destinations = Destination.objects.filter(user=user_in_view)
+      context['destinations'] = destinations
+      recommendations = Recommendation.objects.filter(user=user_in_view)
+      context['recommendations'] = recommendations
+      return context
